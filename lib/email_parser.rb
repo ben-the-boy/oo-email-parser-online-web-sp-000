b66fd4c8ef6c12feac844a01ef54519e03fa2f9a
Ben Hayes <b.t.hayes@comcast.net>
2021-01-04 21:59:17 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,6 +4,7 @@
 # or whitespace (' ').
 
 class EmailAddressParser
+  
   attr_accessor :email_addresses
   
   def initialize(email_addresses)
@@ -11,6 +12,8 @@ class EmailAddressParser
   end
   
   def parse
+    address_array = @email_addresses.split(/, /)
+    address_array
   end
   
 end
